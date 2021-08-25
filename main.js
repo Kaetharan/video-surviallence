f47fe7e368ddbdc664fee9052cc76f94cf465dd0
Kaetharan <78970710+Kaetharan@users.noreply.github.com>
2021-08-25 12:42:39 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video="";
 status="";
+objects=[];
 function setup(){
     canvas=createCanvas(450,380);
     canvas.center();
@@ -12,6 +13,21 @@ function preload(){
 
 function draw(){
     image(video, 0, 0, 450, 380);
+    if(status != ""){
+      objectDetector.detect(video,gotResult);
+
+      for(i=0; i < objects.length; i++){
+         document.getElementById("status").innerHTML="Status: Objects Detected";
+         document.getElementById("number_of_items").innerHTML= "Number of Objects Are: " + objects.length;
+
+         fill(255,0,0);
+         percent=floor(objects[i].confidence*100);
+         text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+         noFill();
+         stroke(255, 0, 0);
+         rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      }
+    }
 }
 
 function Start(){
@@ -25,4 +41,13 @@ function modelLoaded(){
     video.loop(); 
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error, results){
+ if(error){
+     console.log(error);
+ }else{
+     console.log(results);
+     objects=results;
+ }
 }
